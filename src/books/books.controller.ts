fb5390ad2b53906e7a3229152c9059a0be9c7631
Fred Mainga <fred.mainga@lipad.io>
2022-08-04 21:12:51 +0300

    Hotfix - Added Swagger Docs

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,17 +1,21 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Get, Param, Post, Request } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Request } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { BooksService } from './books.service';
-
+import { CharactersQueryParams } from './dtos/characters-query-params.dto';
+@ApiTags("Endpoints")
 @Controller("api")
 export class BooksController {
   constructor(private booksService: BooksService) {}
   @Get('books')
-  fetchAllBooks(@Request() req: any){
+  @ApiOperation({summary:"Fetching Books from API and Syncing with the database"})
+  @ApiResponse({ status: 201, description: 'Resources Found' })
+  fetchAllBooks(){
     return this.booksService.fetchAllBooks()
   }
   @Get('book-characters/:bookID')
-  fetchBooksCharacterList(@Param ('bookID') bookID: number){
-    return this.booksService.fetchBookCharacterList(bookID)
+  fetchBooksCharacterList(@Query() params: CharactersQueryParams, @Param ('bookID') bookID: number){
+    return this.booksService.fetchBookCharacterList(params, bookID)
   }
   @Get('comments/:bookID')
   fetchBooksComments(@Param ('bookID') bookID: number){
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Top Up Mama Test')
+    .setDescription('TUM Test API Specification')
+    .setVersion('1.0')
+    .addTag('Top Up Mama Test APIs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const port = process.env.PORT;
   await app.listen(port, () => {
     console.log('Application Started on Port:', port);
